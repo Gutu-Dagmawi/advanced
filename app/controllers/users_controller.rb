ff6957a51c91c94a10e7b756e5c25dfe6da1d464
Dagmawi Gutu <dagigutu@gmail.com>
2025-07-25 07:59:17 +0300

    second commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,16 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find_by(id: params[:id])
+    if @user
+      render json: @user, status: :ok
+    else
+      render json: { error: "User not found" }, status: :not_found
+    end
   end
 
   def create
-    @user = User.new(email: params[:email], password: params[:password])
+    @user = User.new(user_params)
     if @user.save
       render json: @user, status: :created
     else
@@ -17,8 +23,32 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find_by(id: params[:id])
+    if @user
+      if @user.update(email: params[:email], password: params[:password])
+        render json: @user, status: :ok
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    else
+      render json: { error: "User not found" }, status: :not_found
+    end
   end
 
   def destroy
+    @user = User.find_by(id: params[:id])
+    if @user
+      @user.destroy
+      head :no_content
+    else
+      render json: { error: "User not found" }, status: :not_found
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,10 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   let!(:users) { create_list(:user, 2) }
-  before(:each) do
-    @user = users.first
-  end
-  after(:each) do
+  after :each do
     User.destroy_all
   end
 
@@ -20,8 +17,13 @@ RSpec.describe "Users", type: :request do
 
   describe "GET /users/:id" do
     it "returns a single user" do
-      get "/users/:id"
-      expect(response).to have_http_status(:success)
+      user = users.first
+      get "/users/#{user.id}"
+
+      parsed = JSON.parse(response.body)
+      expect(response).to have_http_status(:ok)
+      expect(parsed["id"]).to eq(user.id)
+      expect(parsed["email"]).to eq(user.email)
     end
   end
 
@@ -29,22 +31,27 @@ RSpec.describe "Users", type: :request do
     let(:valid_params) { { email: "testUser@bitscollege.edu.et", password: "password123" } }
     it "creates a user" do
       post "/users", params: valid_params
-      expect(response).to have_http_status(201)
+      expect(response).to have_http_status(:created)
       expect(User.count).to eq(3)
     end
   end
 
   describe "PATCH /users" do
     it "updates a single user" do
-      patch "/users/:id"
-      expect(response).to have_http_status(:success)
+      user = users.first
+      patch "/users/#{user.id}", params: { email: "updatedUser@bitscollege.edu.et" }
+      expect(response).to have_http_status(:ok)
+      user.reload
+      expect(user.email).to eq("updatedUser@bitscollege.edu.et")
     end
   end
 
   describe "DELETE /users" do
     it "deletes a single user" do
-      get "/users/:id"
-      expect(response).to have_http_status(:success)
+      user = users.first
+      delete "/users/#{user.id}"
+      expect(response).to have_http_status(:no_content)
+      expect(User.exists?(user.id)).to be_falsey
     end
   end
 end
